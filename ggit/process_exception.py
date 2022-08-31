f3793eba1fa746b84eb3aa7e8876df62e87d842a
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2022-08-31 19:16:51 +0200

    better process exception

--- ggit/process_exception.py
+++ ggit/process_exception.py
@@ -1,5 +1,8 @@
 class ProcessException(Exception):
+
     def __init__(self, message):
         self.message = message
+        super().__init__(self.message)
+
     def __str__(self):
         return self.message
