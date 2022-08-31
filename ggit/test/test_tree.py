6753c387bbd5af519e91813d276205748f49aa97
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2022-08-31 19:16:10 +0200

    tree tested

--- ggit/test/test_tree.py
+++ ggit/test/test_tree.py
@@ -1,28 +1,76 @@
-from ast import main
-import sys
+import os
+import subprocess
 from pathlib import Path
 
-from rich.console import Console
-
-sys.path.append(Path(__file__).parent.parent.parent.__str__())
-
+from ggit.converter.model.hash_type import HashType
 from ggit.converter.git_connector import GitConnectorSubprocess
 from ggit.entities import Blob, Tree
-from ggit.utils import walk_folder_flat
+from ggit.utils import walk_objects, walk_folder_flat
+from rich.console import Console
 
-main_tree = Tree()
+repo = Path(__file__).parent.joinpath('assets', 'tree_tester')
+os.chdir(repo)
+subprocess.run(['git', 'init'], stdout=subprocess.PIPE)
+subprocess.run(['git', 'add', '.'], stdout=subprocess.PIPE)
+subprocess.run(['git', 'commit', '-m', 'test'], stdout=subprocess.PIPE)
 
-sub_tree = Tree()
-with open(Path(__file__).parent.parent.parent.joinpath('git_test').joinpath('folder').joinpath("asd.txt"), 'rb') as f:
-    blob1 = Blob(f.read())
-    sub_tree.append_item(blob1, "asd.txt", "100644")
+connector = GitConnectorSubprocess()
 
-main_tree.append_item(sub_tree, "folder", "040000")
+# getting tree hashes from test repository
+trees = [obj for obj in connector.get_all_objects() if obj[1] == HashType.TREE]
 
-with open(Path(__file__).parent.parent.parent.joinpath('git_test').joinpath("asd2.txt"), 'rb') as f:
-    blob2 = Blob(f.read())
-    main_tree.append_item(blob2, "asd2.txt", "100644")
+# identifying the trees
+cat_process = subprocess.run(['git', 'cat-file', '-p', trees[0][0]], stdout=subprocess.PIPE)
+main_tree = None
+sub_tree = None
+if 'sub' in cat_process.stdout.decode('utf-8'):
+    main_tree_hash_real = trees[0][0]
+    sub_tree_hash_real = trees[1][0]
+else:
+    main_tree_hash_real = trees[1][0]
+    sub_tree_hash_real = trees[0][0]
 
-print("100644 blob " + blob2.hash + "\t" + "asd2.txt")
-print("040000 tree " + sub_tree.hash + "\t" + "folder")
-print(main_tree.hash)
+items_sub = []
+for i in walk_folder_flat(repo.joinpath('sub')):
+    if os.access(i, os.X_OK):
+        mode = '100755'
+    elif i.is_symlink():
+        mode = '120000'
+    else:
+        mode = '100644'
+    items_sub.append((Blob(i.read_bytes()), i.name, mode))
+
+sub_tree = Tree(items_sub)
+items_main = []
+for i in walk_folder_flat(repo):
+    if os.access(i, os.X_OK):
+        mode = '100755'
+    elif i.is_symlink():
+        mode = '120000'
+    else:
+        mode = '100644'
+    items_main.append((Blob(i.read_bytes()), i.name, mode))
+items_main.append((sub_tree, 'sub', '040000'))
+main_tree = Tree(items_main)
+
+# comparing the trees
+console = Console()
+console.print(f"[blue]main_tree[/blue]")
+if main_tree.hash == main_tree_hash_real:
+    console.print(f"[green]{main_tree_hash_real} -> {main_tree.hash}[/green]")
+else:
+    console.print(f"[red]{main_tree_hash_real} -> {main_tree.hash}[/red]")
+assert main_tree.hash == main_tree_hash_real
+
+console.print(f"[blue]sub_tree[/blue]")
+if sub_tree.hash == sub_tree_hash_real:
+    console.print(f"[green]{sub_tree_hash_real} -> {sub_tree.hash}[/green]")
+else:
+    console.print(f"[red]{sub_tree_hash_real} -> {sub_tree.hash}[/red]")
+assert sub_tree.hash == sub_tree_hash_real
+
+
+
+
+
+subprocess.run(['rm', '-rf', '.git'])
