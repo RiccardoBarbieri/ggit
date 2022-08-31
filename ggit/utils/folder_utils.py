f08e68de30dc0e24e28aae94baf3976bacfebd3d
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2022-08-31 19:19:50 +0200

    added walk folder flat (no rec), only returns files, not dirs

--- ggit/utils/folder_utils.py
+++ ggit/utils/folder_utils.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 from typing import List
-
+import typing
 
 def walk_objects(repository) -> List[Path]:
     """
@@ -25,50 +25,7 @@ def walk_objects(repository) -> List[Path]:
                 files.append(Path.joinpath(Path(i[0]), j))
     return files
 
-# def walk_folder_flat(folder: Path, get: str = 'files') -> List[Path]:
-#     """
-#     Walk a folder and by default returns all files found.
-#     If get is set to 'folders' it will return all folders found, and
-#     if get is set to 'both' it will return both files and folders.
-#     The files are returned as a list of Path objects.
-
-#     Parameters
-#     ----------
-#     folder: Path
-#         The folder to walk.
-#     get: str, optional
-#         The type of files to return. Can be 'files', 'folders' or 'both'.
-
-#     Returns
-#     -------
-#     List[Path]
-#         The list of Path objects of the files found.
-#     """
-#     if get not in ['files', 'folders', 'both']:
-#         raise ValueError("get must be 'files', 'folders' or 'both'")
-#     files = []
-#     walk = os.walk(folder)
-#     if get == 'files':
-#         for i in walk:
-#             if i[2]:
-#                 for j in i[2]:
-#                     files.append(Path.joinpath(Path(i[0]), j))
-#     elif get == 'folders':
-#         for i in walk:
-#             if i[1]:
-#                 for j in i[1]:
-#                     files.append(Path.joinpath(Path(i[0]), j))
-#     elif get == 'both':
-#         for i in walk:
-#             if i[2]:
-#                 for j in i[2]:
-#                     files.append(Path.joinpath(Path(i[0]), j))
-#             if i[1]:
-#                 for j in i[1]:
-#                     files.append(Path.joinpath(Path(i[0]), j))
-#     return files
-
-def walk_folder_flat(folder: Path) -> List[Path]:
+def walk_folder_rec_flat(folder: Path) -> typing.Generator[Path, None, None]:
     """
     Walk a folder and returns all files found.
     The files are returned as a list of Path objects.
@@ -83,10 +40,27 @@ def walk_folder_flat(folder: Path) -> List[Path]:
     List[Path]
         The list of Path objects of the files found.
     """
-    files = []
     walk = os.walk(folder)
     for i in walk:
         if i[2]:
             for j in i[2]:
-                files.append(Path.joinpath(Path(i[0]), j))
-    return files
+                yield Path.joinpath(Path(i[0]), j)
+
+def walk_folder_flat(folder: Path) -> typing.Generator[Path, None, None]:
+    """
+    Walk a folder and returns all files found at the first level.
+    The files are returned as a list of Path objects.
+
+    Parameters
+    ----------
+    folder: Path
+        The folder to walk.
+
+    Returns
+    -------
+    List[Path]
+        The list of Path objects of the files found.
+    """
+    for i in os.listdir(folder):
+        if os.path.isfile(Path.joinpath(folder, i)):
+            yield Path.joinpath(folder, i)
