38094d2c39d7d382df512d447b05f58b67c2f508
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2022-09-03 15:32:30 +0200

    created singleton metaclass to create singleton instances of certain classes

---
+++ ggit/utils/singleton.py
@@ -0,0 +1,19 @@
+
+
+class SingletonMeta(type):
+    """
+    Metaclass to manage a singleton instance of a class.
+    """
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        """
+        Possible changes to the value of the `__init__` argument do not affect
+        the returned instance.
+        """
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
