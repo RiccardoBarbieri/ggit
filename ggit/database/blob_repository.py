0ea7bbb8f242a95b830d2069ccf1832943675ca7
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2022-09-05 19:34:58 +0200

    completed blob repository

--- ggit/database/blob_repository.py
+++ ggit/database/blob_repository.py
@@ -4,6 +4,7 @@ if TYPE_CHECKING:
     from ggit.database import DataSource
 
 from ggit.entities import Blob
+from neo4j import Query, Record, Result
 
 
 class BlobRepository:
@@ -14,16 +15,37 @@ class BlobRepository:
         self.data_source = data_source
 
     def add_blob(self, blob: Blob) -> bool:
-        pass
+        with self.data_source.new_session() as session:
+            result = session.run(
+                "CREATE (b:Blob {hash: $hash, content: $content, length: $length}) RETURN b", hash=blob.hash, content=blob.content, length=blob.length)
+            return result.consume().counters.nodes_created == 1
 
-    def get_blob(self, sha1: str) -> Blob:
-        pass
+    def get_blob(self, hash: str) -> Blob:
+        with self.data_source.new_session() as session:
+            result = session.run(
+                "MATCH (blob:Blob {hash: $hash}) RETURN blob", hash=hash)
+            if result.single() is None:
+                return None
+            return Blob(result.single()['blob']['content'])
 
     def get_all_blobs(self) -> List[Blob]:
-        pass
+        with self.data_source.new_session() as session:
+            result: Result = session.run("MATCH (blob:Blob) RETURN blob")
+            return [Blob(record['blob']['content']) for record in result]
 
-    def delete_blob(self, sha1: str) -> bool:
-        pass
+    def delete_blob(self, hash: str) -> bool:
+        with self.data_source.new_session() as session:
+            result = session.run(
+                "MATCH (blob:Blob {hash: $hash}) DELETE blob", hash=hash)
+            return result.consume().counters.nodes_deleted == 1
 
-    def update_blob(self, blob: Blob) -> bool:
-        pass
+if __name__ == '__main__':
+    from ggit.database import DataSource
+
+    data = DataSource()
+    a = BlobRepository(data)
+
+
+
+
+    
