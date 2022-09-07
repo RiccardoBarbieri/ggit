aa2c0ab733e8c246fa7136820123fc28d9c9e5bc
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2022-09-07 17:00:13 +0200

    dark mode

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 master_doc = 'index'
 
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_dark_mode'
 default_dark_mode = True
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
