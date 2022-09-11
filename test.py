b35aca320d75c300e237ae9a6e4a67ce7c6cccc8
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2022-09-11 20:17:24 +0200

    misc tests

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import logging
+import logging.config
 import mimetypes
 import os
 import subprocess
@@ -7,9 +9,9 @@ import hashlib
 from datetime import datetime, timedelta, timezone
 import traceback
 
-from ggit.utils.folder_utils import walk_folder_rec_flat
+from ggit.utils.folder_utils import walk_folder_rec_flat, walk_objects
 
-os.chdir(Path(__file__).parent.joinpath("ggit", "test", "assets", "tree_tester"))
+# os.chdir(Path(__file__).parent.joinpath("ggit", "test", "assets", "tree_tester"))
 
 body = b"""tree ff7ae585339f04217663bdb7f54df531c6fb8217
 author RiccardoBarbieri <riccardo_barbieri@outlook.it> 1662040308 +0200
@@ -20,4 +22,6 @@ test
 
 # print(hashlib.sha1(b"commit " + str(len(body)).encode("ascii") + b"\0" + body).hexdigest())
 
-print('hello world')
+old_path = Path("test.txt")
+
+new_path = Path("folder/test.txt")
