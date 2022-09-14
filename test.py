f2122da3ece2dcfa0d527959a60dfc7932aa0b21
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2022-09-14 17:23:04 +0200

    misc test

--- test.py
+++ test.py
@@ -21,19 +21,3 @@ test
 """
 
 # print(hashlib.sha1(b"commit " + str(len(body)).encode("ascii") + b"\0" + body).hexdigest())
-
-a = """Directories in use:
-home:         /home/riccardoob/thesis/.ggit/neo4j-community-4.4.10
-config:       /home/riccardoob/thesis/.ggit/neo4j-community-4.4.10/conf
-logs:         /home/riccardoob/thesis/.ggit/neo4j-community-4.4.10/logs
-plugins:      /home/riccardoob/thesis/.ggit/neo4j-community-4.4.10/plugins
-import:       /home/riccardoob/thesis/.ggit/neo4j-community-4.4.10/import
-data:         /home/riccardoob/thesis/.ggit/neo4j-community-4.4.10/data
-certificates: /home/riccardoob/thesis/.ggit/neo4j-community-4.4.10/certificates
-licenses:     /home/riccardoob/thesis/.ggit/neo4j-community-4.4.10/licenses
-run:          /home/riccardoob/thesis/.ggit/neo4j-community-4.4.10/run
-Starting Neo4j.
-Started neo4j (pid:11894). It is available at http://0.0.0.0:7474
-There may be a short delay until the server is ready."""
-
-pid = a.splitlines()[-2].split("(pid:")[1].split(")")[0]
